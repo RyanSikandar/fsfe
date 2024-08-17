8de874b733d6961d13f17fe342f51020fee5303e
RyanSikandar <ryansikandar@gmail.com>
2024-08-17 23:50:38 +0500

    a few changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer(function (req,res){
 
-res.write("full stack kinda shit");
+res.write("I am a simple node server from ryansikandar.me");
 	res.end();
 }).listen(3000);
 
