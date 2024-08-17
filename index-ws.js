8fddc60f876b86243438636838a6ae52177192a4
RyanSikandar <ryansikandar@gmail.com>
2024-08-18 02:48:42 +0500

    added database and stored client information

--- index-ws.js
+++ index-ws.js
@@ -8,6 +8,18 @@ app.get('/', function (req, res) {
     });
 });
 
+//catching the interrupt signal which is Ctrl+C 
+process.on('SIGINT', function () {
+    console.log("Caught interrupt signal");
+    wss.clients.forEach(function each(client) {
+        client.close();
+    });
+    server.close(() => {
+        shutdownDB();
+    });
+
+})
+
 server.on('request', app);
 
 server.listen(3000, function () {
@@ -29,6 +41,7 @@ wss.on('connection', function connection(ws) {
     if (ws.readyState === ws.OPEN) {
         ws.send("Welcome to the chat room!");
     }
+    db.run(`INSERT INTO visitors (count, time) VALUES (${numClients}, datetime('now'))`);
 
     ws.on('close', function close() {
         console.log("Client disconnected");
@@ -41,4 +54,35 @@ wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
         client.send(data);
     });
+}
+
+//end websockets
+
+//begin database code yipee
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:'); // Create an in-memory database
+
+// this command runs before any other command to ensure the database is ready to be used.
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+            `)
+})
+
+function getCounts() {
+    db.each(
+        "SELECT * FROM visitors", (err, row) => {
+            console.log(row);
+        }
+    )
+}
+
+function shutdownDB() {
+    console.log("Getting counts from database");
+    getCounts();
+    console.log("Shutting down database");
+    db.close();
 }
