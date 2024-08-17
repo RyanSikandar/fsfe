bfa7784b7f226f7fcdc6be5b098b52972979b51e
RyanSikandar <ryansikandar@gmail.com>
2024-08-18 00:43:00 +0500

    added websocket server

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require('http');
 
-http.createServer(function (req,res){
+http.createServer(function (req, res) {
 
-res.write("I am a simple node server from ryansikandar.me");
+	res.write("I am a simple node server from ryansikandar.me");
 	res.end();
 }).listen(3000);
 
 console.log("server started");
+
--- index-ws.js
+++ index-ws.js
@@ -2,14 +2,43 @@ const express = require('express');
 const server = require('http').createServer();
 const app = express();
 
-app.get('/',function(req,res){
-    res.sendFile('index.html',{
-        root:__dirname
+app.get('/', function (req, res) {
+    res.sendFile('index.html', {
+        root: __dirname
     });
 });
 
-server.on('request',app);
+server.on('request', app);
 
-server.listen(3000,function(){
+server.listen(3000, function () {
     console.log("Server started on port 3000")
 })
+
+//Begin websockets wohoo
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({
+    server: server
+});
+
+wss.on('connection', function connection(ws) {
+
+    const numClients = wss.clients.size;
+    console.log("Clients connected: " + numClients);
+    wss.broadcast("New client connected. Total clients: " + numClients);
+    if (ws.readyState === ws.OPEN) {
+        ws.send("Welcome to the chat room!");
+    }
+
+    ws.on('close', function close() {
+        console.log("Client disconnected");
+        wss.broadcast("Client disconnected. Total clients: " + wss.clients.size);
+    })
+
+});
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data);
+    });
+}
